bcebe0609d53831e493663ed9e73eb02babed713
code-bbob <officialbibhab@gmail.com>
2023-09-30 14:06:20 +0545

    bibhab ko

--- backend/blog/urls.py
+++ backend/blog/urls.py
@@ -20,7 +20,8 @@ from . import views
 
 urlpatterns = [
     path('', views.blogHome, name='blogHome'),
+    path('postComment', views.postComment, name="postComment"),
     path('<str:slug>', views.blogPost, name='blogPost'),
-    path('post/', views.post, name='post'),
+
 ]
  
